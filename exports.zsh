81a1ee9ac8e246de16c8964da256b84580c7c455
Sven Günther <sven.guenther@it-agile.de>
2016-02-10 18:59:30 +0100

    new colors, new exports

--- exports.zsh
+++ exports.zsh
@@ -21,10 +21,6 @@ export LESS='--ignore-case --raw-control-chars'
 export PAGER='less'
 export EDITOR='subl -w'
 
-#export NODE_PATH=/opt/github/homebrew/lib/node_modules
-#export PYTHONPATH=/usr/local/lib/python2.6/site-packages
 # CTAGS Sorting in VIM/Emacs is better behaved with this in place
 export LC_COLLATE=C
-
-#export GH_ISSUE_CREATE_TOKEN=083f60c674d8eb41f98258df9fc8d94cb733218a
 
